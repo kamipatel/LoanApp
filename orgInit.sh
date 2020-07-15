21012f81d59fccc1eaabd6563d1b057e12b02ffd
kamipatel@gmail.com <kamipatel@gmail.com>
2020-07-15 10:01:23 -0500

    updates

--- orgInit.sh
+++ orgInit.sh
@@ -2,10 +2,11 @@
 
 sfdx force:org:create -f config/project-scratch-def.json -a ADK --setdefaultusername -d 1
 
-sfdx force:source:push 
-
-sfdx force:user:permset:assign -n loanapp 
-
 sfdx force:apex:execute -f config/create-demo-data-setup.apex
 
 sfdx force:org:open
+
+# only after enabling dynamic forms and path in scratch org
+# sfdx force:source:push 
+# sfdx force:user:permset:assign -n loanapp 
+
